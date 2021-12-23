1a1a859235700cd4299423205976119a5d930870
Manohar Sharma <manoharsha10@gmail.com>
2021-12-23 07:43:35 +0545

    urls

--- home/urls.py
+++ home/urls.py
@@ -1,11 +1,10 @@
-import django
 from django.urls import path
 
 from .views import *
 
 app_name = 'home'
 urlpatterns = [
-    django.urls.path('', HomeView.as_view(), name='home'),
-    django.urls.path('subcategory/<slug>', SubCategoryView.as_view(), name='subcategory'),
-    django.urls.path('detail/<slug>', ItemDetailView.as_view(), name='detail'),
+    path('', HomeView.as_view(), name='home'),
+    path('subcategory/<slug>', SubCategoryView.as_view(), name='subcategory'),
+    path('detail/<slug>', ItemDetailView.as_view(), name='detail'),
 ]
